5ae65cdc18cc24242164feb638c0a29a721843a4
Matveirt <147849880+Matveirt@users.noreply.github.com>
2023-10-16 20:54:03 +0300

    Delete test UkPyScript directory

--- test UkPyScript/test1.py
+++
@@ -1,6 +0,0 @@
----input---
-import UkPyScript as UPS
-test1 = UPS.binary.init()
-
----output---
-code enabled %100 errors 0
--- test UkPyScript/test2.py
+++
@@ -1,8 +0,0 @@
---input--
-import UkPyScript as UPS
-UPS.binary.init()
-UPS.binary.include("LIN")
-LIN.init()
---output--
-code enabled %100 errors 0
-LIN errors 0
